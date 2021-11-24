c885ad403f4bf12471662b0eb68bcb5b00fa0246
Simon Gredal <simon.gredal@icloud.com>
2021-11-24 11:18:01 +0100

    Tests?

---
+++ src/test/java/gruppe8/nebula/NebulaApplicationTests.java
@@ -0,0 +1,13 @@
+package gruppe8.nebula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NebulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
