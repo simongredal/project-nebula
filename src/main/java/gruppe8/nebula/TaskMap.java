34fba3c0f63c38c7c15cc8b37ca78a29d622ba4b
Simon Gredal <simon.gredal@icloud.com>
2021-11-24 12:46:30 +0100

    initial commit

---
+++ src/main/java/gruppe8/nebula/TaskMap.java
@@ -0,0 +1,11 @@
+package gruppe8.nebula;
+
+import gruppe8.nebula.models.Task;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TaskMap extends HashMap<Long, Task> implements Map<Long, Task> {
+
+
+}
