80dd3e3e7d39d9c8d2823c6ad84aff27b6264066
Simon Gredal <simon.gredal@icloud.com>
2021-11-24 13:42:31 +0100

    sql better matches domain model

--- src/main/resources/sql-script/database.sql
+++ src/main/resources/sql-script/database.sql
@@ -1,108 +1,46 @@
 BEGIN;
-set foreign_key_checks = 0;
+SET foreign_key_checks = 0;
+DROP TABLE IF EXISTS accounts;
+DROP TABLE IF EXISTS teams;
+DROP TABLE IF EXISTS memberships;
+DROP TABLE IF EXISTS projects;
+DROP TABLE IF EXISTS tasks;
+SET foreign_key_checks = 1;
 
-DROP TABLE IF EXISTS Accounts;
-DROP TABLE IF EXISTS Projects;
-DROP TABLE IF EXISTS Subprojects;
-DROP TABLE IF EXISTS Tasks;
-DROP TABLE IF EXISTS Teams;
-
-set foreign_key_checks = 1;
-
-
-
-create table Accounts
+CREATE TABLE accounts
 (
-    ID       int auto_increment,
-    name     varchar(255) not null,
-    password varchar(255) not null,
-    email    varchar(255) not null,
-    constraint Account_ID_uindex
-        unique (ID),
-    constraint Accounts_email_uindex
-        unique (email)
+    id       int AUTO_INCREMENT PRIMARY KEY,
+    email    varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    name     varchar(255) NOT NULL,
+    CONSTRAINT accounts_email_uindex UNIQUE (email)
 );
-
-alter table Accounts
-    add primary key (ID);
-
-create table Teams
+CREATE TABLE teams
 (
-    ID        int auto_increment,
-    Project   varchar(255) not null,
-    AccountID int          null,
-    TeamName  varchar(255) not null,
-    constraint Team_ID_uindex
-        unique (ID),
-    constraint Team_Project_uindex
-        unique (Project),
-    constraint Teams_TeamName_uindex
-        unique (TeamName),
-    constraint fk_AccountID
-        foreign key (AccountID) references Accounts (ID)
-            on delete cascade
+    id   int AUTO_INCREMENT PRIMARY KEY,
+    name varchar(255) NOT NULL
 );
-
-alter table Teams
-    add primary key (ID);
-
-create table Projects
+CREATE TABLE memberships
 (
-    ID          int auto_increment,
-    ProjectName varchar(255) not null,
-    TeamID      int          null,
-    constraint Project_ID_uindex
-        unique (ID),
-    constraint Project_ProjectName_uindex
-        unique (ProjectName),
-    constraint fk_TeamID
-        foreign key (TeamID) references Teams (ID)
-            on delete cascade
+    id         int AUTO_INCREMENT PRIMARY KEY,
+    team_id    int    NOT NULL,
+    account_id int    NOT NULL,
+    accepted   binary NULL,
+    CONSTRAINT team_id_fkindex FOREIGN KEY (team_id) REFERENCES teams (id) ON DELETE CASCADE,
+    CONSTRAINT account_id_fkindex FOREIGN KEY (account_id) REFERENCES accounts (id)
 );
-
-alter table Projects
-    add primary key (ID);
-
-create table Subprojects
+CREATE TABLE projects
 (
-    ID             int auto_increment,
-    Assignment     varchar(255) not null,
-    ProjectID      int          null,
-    SubprojectName varchar(255) not null,
-    constraint Subproject_ID_uindex
-        unique (ID),
-    constraint Subproject_SubprojectName_uindex
-        unique (SubprojectName),
-    constraint fk_ProjectID
-        foreign key (ProjectID) references Projects (ID)
-            on delete cascade
+    id   int AUTO_INCREMENT PRIMARY KEY,
+    name varchar(255) NOT NULL
 );
-
-alter table Subprojects
-    add primary key (ID);
-
-create table Tasks
+CREATE TABLE tasks
 (
-    ID           int auto_increment,
-    ProjectID    int          null,
-    SubprojectID int          null,
-    AccountID    int          null,
-    TaskName     varchar(255) not null,
-    AccountName  varchar(255) null,
-    constraint Tasks_ID_uindex
-        unique (ID),
-    constraint fkAccountID
-        foreign key (AccountID) references Accounts (ID)
-            on delete cascade,
-    constraint fkProjectID
-        foreign key (ProjectID) references Projects (ID)
-            on delete cascade,
-    constraint fkSubprojectID
-        foreign key (SubprojectID) references Subprojects (ID)
-            on delete cascade
+    id         int AUTO_INCREMENT PRIMARY KEY,
+    project_id int          NOT NULL,
+    parent_id  int          NULL,
+    name       varchar(255) NULL,
+    CONSTRAINT project_id_fkindex FOREIGN KEY (project_id) REFERENCES projects (id) ON DELETE CASCADE,
+    CONSTRAINT parent_id_fkindex FOREIGN KEY (parent_id) REFERENCES tasks (id) ON DELETE CASCADE
 );
-
-alter table Tasks
-    add primary key (ID);
-
-commit;
+COMMIT;
