900103c639dde2a72bc0967dc8b2ee432413989f
MaltheGram <malthegram22@gmail.com>
2021-11-24 13:44:34 +0100

    pushing lots of shit

--- src/main/java/gruppe8/nebula/SignupController/SignupController.java
+++ src/main/java/gruppe8/nebula/SignupController/SignupController.java
@@ -1,23 +1,46 @@
 package gruppe8.nebula.SignupController;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @Controller
 public class SignupController {
+    private final SignupService signupService;
+    private final Logger logger;
+
+    public SignupController(SignupService registrationService) {
+        this.signupService = registrationService;
+        this.logger = LoggerFactory.getLogger(this.getClass());
+    }
 
     @GetMapping("/signup")
-    public String signupPage(){
+    public String signup(@RequestParam Optional<String> error, Model model) {
+        if (error.isPresent()) {
+            model.addAttribute("error", true);
+        }
         return "signup";
     }
 
+    @PostMapping("/signup")
+    public String addUser(SignupRequest request) {
+        logger.info("Received SignupRequest");
+        Boolean success = signupService.register(request);
 
+        if (success) {
+            logger.info("Successful signup");
+            return "redirect:/login?signupSuccess";
+        }
 
-
-
-
+        logger.info("Unsuccessful signup");
+        return "redirect:/signup?error";
+    }
 
 }
-
---
+++ src/main/java/gruppe8/nebula/controllers/WebSecurity.java
@@ -0,0 +1,56 @@
+package gruppe8.nebula.controllers;
+
+import gruppe8.nebula.repositories.AccountRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+    private final AccountRepository accountRepository;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    public WebSecurity(AccountRepository accountRepository){
+        this.accountRepository = accountRepository;
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .anyRequest().permitAll();
+    }
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
+
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setPasswordEncoder(new Argon2PasswordEncoder());
+        authenticationProvider.setUserDetailsService(accountRepository);
+        return authenticationProvider;
+    }
+
+    @Bean
+    public Argon2PasswordEncoder passwordEncoder() {
+        return new Argon2PasswordEncoder(16, 32 , 1, 32768, 5);
+    }
+}
