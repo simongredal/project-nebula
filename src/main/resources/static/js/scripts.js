f15c1de0994a42f1db336bdb8e208b5509d2dbbe
Mark Friis Larsen <marklarsen96@gmail.com>
2021-11-24 12:37:40 +0100

    added add row to table js

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -10,6 +10,15 @@ function passwordVisibility(){
     }
 }
 
+function addRowToTable(elemId) {
+    var totalRowCount = elem.rows.length; // 5
+    var row = table.insertRow(totalRowCount);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = " ";
+    cell2.innerHTML = " ";
+}
 function fake(){
     return 2;
 }
+
