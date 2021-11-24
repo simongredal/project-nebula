999242fb5ac79b9275fb1a21f8805e9ba1b5e7e1
Simon Gredal <simon.gredal@icloud.com>
2021-11-24 13:58:43 +0100

    signupshit

--- src/main/java/gruppe8/nebula/SignupController/SignupRequest.java
+++ src/main/java/gruppe8/nebula/SignupController/SignupRequest.java
@@ -1,32 +1,4 @@
 package gruppe8.nebula.SignupController;
 
-/**
- * This record is equivalent to the following class
- *
- *  <pre>
- *  public class SignupRequest {
- *      private String fullName;
- *      private String email;
- *      private String password;
- *
- *      public SignupRequest(String fullName, String email, String password) {
- *      }
- *
- *      public fullName() {
- *          return this.fullName;
- *      }
- *
- *      public email() {
- *          return this.email;
- *      }
- *
- *      public password() {
- *          return this.password;
- *      }
- *  }
- *  </pre>
- *
- */
-
-public record SignupRequest(String fullName, String email, String password) {
+public record SignupRequest(String name, String email, String password) {
 }
