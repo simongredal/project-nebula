33ea0f4ebfc135ef58ccfb356ac78735259a3d52
frederikmahipal <fred88n5@stud.kea.dk>
2021-11-25 13:57:27 +0100

    Create & delete project

--- src/main/java/gruppe8/nebula/models/Project.java
+++ src/main/java/gruppe8/nebula/models/Project.java
@@ -6,21 +6,38 @@ import gruppe8.nebula.entities.TaskEntity;
 import java.util.*;
 
 public class Project {
-    private Long id;
-    private String name;
+
+    private static Long id;
+    private static String name;
     private final List<Task> subtasks = new ArrayList<>();
 
     public Project() {
     }
 
     public void setProjectEntity(ProjectEntity projectEntity) {
-        this.id = projectEntity.id();
-        this.name = projectEntity.name();
+        id = projectEntity.id();
+        name = projectEntity.name();
     }
 
     public void setSubtasks(List<TaskEntity> tasks) {
         tasks.stream()
                 .filter(taskEntity -> taskEntity.parent() == 0L )
                 .forEach( t -> this.subtasks.add(new Task(t, tasks)));
+    }
+
+    public static Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public static String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
--- src/main/java/gruppe8/nebula/services/ProjectService.java
+++ src/main/java/gruppe8/nebula/services/ProjectService.java
@@ -2,10 +2,14 @@ package gruppe8.nebula.services;
 
 import gruppe8.nebula.entities.ProjectEntity;
 import gruppe8.nebula.entities.TaskEntity;
+import gruppe8.nebula.models.Account;
 import gruppe8.nebula.models.Project;
 import gruppe8.nebula.repositories.ProjectRepository;
+import gruppe8.nebula.requests.CreateProjectRequest;
+import gruppe8.nebula.requests.DeleteProjectRequest;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -27,5 +31,19 @@ public class ProjectService {
         project.setSubtasks(taskEntities);
 
         return project;
+    }
+
+    public boolean createProject(Account account, CreateProjectRequest request) {
+        Project project = new Project();
+        request.name();
+
+        return projectRepository.createProject(project, account);
+    }
+
+    public boolean deleteProject(Account account, DeleteProjectRequest request) {
+        Project project = new Project();
+                request.ProjectId();
+
+        return projectRepository.deleteProject(project, account);
     }
 }
