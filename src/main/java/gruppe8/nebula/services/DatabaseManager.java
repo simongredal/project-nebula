a2fabd5692dab8bd578c7c7af92ce566d498aa97
Simon Gredal <simon.gredal@icloud.com>
2021-11-25 13:43:29 +0100

    DatabaseManager: Clean up style to match other services

--- src/main/java/gruppe8/nebula/services/DatabaseManager.java
+++ src/main/java/gruppe8/nebula/services/DatabaseManager.java
@@ -1,6 +1,8 @@
 package gruppe8.nebula.services;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 import org.springframework.core.env.Environment;
@@ -15,20 +17,21 @@ public class DatabaseManager {
     private final String url;
     private final String username;
     private final String password;
+    private final Logger log;
 
     // Spring will automatically supply us with the Environment object.
-    // The environment
     private DatabaseManager(Environment env) {
         this.url = env.getProperty("spring.datasource.url");
         this.username = env.getProperty("spring.datasource.username");
         this.password = env.getProperty("spring.datasource.password");
+        this.log = LoggerFactory.getLogger(this.getClass());
     }
 
     public Connection getConnection() {
         try {
             return DriverManager.getConnection(this.url, this.username, this.password);
         } catch (SQLException e) {
-            e.printStackTrace();
+            log.error(e.getMessage());
         }
         return null;
     }
