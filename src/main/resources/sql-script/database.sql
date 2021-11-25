16a54734b36da176347214207362719a19ceaaa2
Simon Gredal <simon.gredal@icloud.com>
2021-11-25 15:41:28 +0100

    SQL: constrain a membership so (team_id + user_id) is unique (i.e. no duplicate invitations)

--- src/main/resources/sql-script/database.sql
+++ src/main/resources/sql-script/database.sql
@@ -1,48 +1,52 @@
-BEGIN;
-SET foreign_key_checks = 0;
-DROP TABLE IF EXISTS accounts;
-DROP TABLE IF EXISTS teams;
-DROP TABLE IF EXISTS memberships;
-DROP TABLE IF EXISTS projects;
-DROP TABLE IF EXISTS tasks;
-SET foreign_key_checks = 1;
+begin;
+set foreign_key_checks = 0;
+drop table if exists accounts;
+drop table if exists teams;
+drop table if exists memberships;
+drop table if exists projects;
+drop table if exists tasks;
+set foreign_key_checks = 1;
 
-CREATE TABLE accounts
+create table accounts
 (
-    id       int AUTO_INCREMENT PRIMARY KEY,
-    email    varchar(255) NOT NULL,
-    password varchar(255) NOT NULL,
-    name     varchar(255) NOT NULL,
-    CONSTRAINT accounts_email_uindex UNIQUE (email)
+    id       int auto_increment primary key,
+    email    varchar(255) not null,
+    password varchar(255) not null,
+    name     varchar(255) not null,
+    constraint accounts_email_uindex unique (email)
 );
-CREATE TABLE teams
+
+create table teams
 (
-    id   int AUTO_INCREMENT PRIMARY KEY,
-    name varchar(255) NOT NULL
+    id   int auto_increment primary key,
+    name varchar(255) not null
 );
-CREATE TABLE memberships
+
+create table memberships
 (
-    id         int AUTO_INCREMENT PRIMARY KEY,
-    team_id    int    NOT NULL,
-    account_id int    NOT NULL,
-    accepted   binary NULL,
-    CONSTRAINT memberships_teams_fkindex FOREIGN KEY (team_id) REFERENCES teams (id) ON DELETE CASCADE,
-    CONSTRAINT memberships_accounts_fkindex FOREIGN KEY (account_id) REFERENCES accounts (id)
+    id         int auto_increment primary key,
+    team_id    int       not null,
+    account_id int       not null,
+    accepted   binary(1) null,
+    constraint memberships_team_account_uindex unique (team_id, account_id),
+    constraint memberships_accounts_fkindex foreign key (account_id) references accounts (id),
+    constraint memberships_teams_fkindex foreign key (team_id) references teams (id) on delete cascade
 );
-CREATE TABLE projects
+
+create table projects
 (
-    id      int AUTO_INCREMENT PRIMARY KEY,
-    team_id int          NOT NULL,
-    name    varchar(255) NOT NULL,
-    CONSTRAINT projects_teams_fkindex FOREIGN KEY (team_id) REFERENCES teams (id) ON DELETE CASCADE
+    id      int auto_increment primary key,
+    team_id int          not null,
+    name    varchar(255) not null,
+    constraint projects_teams_fk foreign key (team_id) references teams (id) on delete cascade
 );
-CREATE TABLE tasks
+
+create table tasks
 (
-    id         int AUTO_INCREMENT PRIMARY KEY,
-    project_id int          NOT NULL,
-    parent_id  int          NULL,
-    name       varchar(255) NULL,
-    CONSTRAINT tasks_projects_fkindex FOREIGN KEY (project_id) REFERENCES projects (id) ON DELETE CASCADE,
-    CONSTRAINT tasks_tasks_fkindex FOREIGN KEY (parent_id) REFERENCES tasks (id) ON DELETE CASCADE
+    id         int auto_increment primary key,
+    project_id int          not null,
+    parent_id  int          null,
+    name       varchar(255) null,
+    constraint tasks_projects_fk foreign key (project_id) references projects (id) on delete cascade,
+    constraint tasks_tasks_fk foreign key (parent_id) references tasks (id) on delete cascade
 );
-COMMIT;
