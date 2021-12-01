d5476b8565ec373aa74cd6ab1fc28dd96df90050
Mark Friis Larsen <marklarsen96@gmail.com>
2021-12-01 17:59:58 +0100

    seppuku

--- src/main/java/gruppe8/nebula/models/Project.java
+++ src/main/java/gruppe8/nebula/models/Project.java
@@ -3,6 +3,10 @@ package gruppe8.nebula.models;
 import gruppe8.nebula.entities.ProjectEntity;
 import gruppe8.nebula.entities.TaskEntity;
 
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 public class Project {
@@ -35,6 +39,49 @@ public class Project {
         List<Task> tasks = new ArrayList<>();
         for (Task task : subtasks) { tasks.addAll(task.getAllTasks()); }
         return tasks;
+    }
+
+    public List<LocalDateTime> getAllDates() {
+        List<LocalDateTime> dates = new ArrayList<>();
+        for (Task task : getAllTasks()) {
+            dates.add(task.getStartDate());
+            dates.add(task.getEndDate());
+        }
+        return dates;
+    }
+
+    public LocalDateTime getStartDate() {
+        LocalDateTime minDate = getAllDates().stream()
+                .min(LocalDateTime::compareTo)
+                .get();
+        return minDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        LocalDateTime maxDate = getAllDates().stream()
+                .max(LocalDateTime::compareTo)
+                .get();
+        return maxDate;
+    }
+
+    public int getTotalProjectSpanDays() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd MM yyyy");
+        String inputString1 = "23 01 1997";
+        String inputString2 = "27 04 1997";
+
+        try {
+            long daysBetween = Duration.between(getStartDate(), getEndDate()).toDays();
+            System.out.println ("Days: " + daysBetween);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+    public List<Date> getTotalProjectSpanDates() {
+        List<Date> projectSpan = new ArrayList<>();
+        projectSpan.add(getStartDate());
+
+
     }
 
     public Long getId() {
