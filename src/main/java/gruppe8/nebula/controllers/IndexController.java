86ac05272761e50d005ef80e75c0f9f991c98a74
Simon Gredal <simon.gredal@icloud.com>
2021-12-01 12:41:59 +0100

    Move contact page into IndexController
    This gives us even nicer annotations in IntelliJ

--- src/main/java/gruppe8/nebula/controllers/IndexController.java
+++ src/main/java/gruppe8/nebula/controllers/IndexController.java
@@ -18,4 +18,9 @@ public class IndexController {
         model.addAttribute("account", account);
         return "index";
     }
+
+    @GetMapping("/contact")
+    public String contact(){
+        return "contact";
+    }
 }
