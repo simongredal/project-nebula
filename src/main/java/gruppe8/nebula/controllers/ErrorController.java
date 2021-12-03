718852e5a30d25937c570b97d8a9d37fb337ab20
MaltheGram <malthegram22@gmail.com>
2021-12-03 13:48:23 +0100

    minor changes to error handling 404

--- src/main/java/gruppe8/nebula/controllers/ErrorController.java
+++ src/main/java/gruppe8/nebula/controllers/ErrorController.java
@@ -6,9 +6,6 @@ package gruppe8.nebula.controllers;
  */
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
