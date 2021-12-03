eb3a888e0a2aeddd0df70aac6881e7df9dd01102
MaltheGram <malthegram22@gmail.com>
2021-12-03 11:20:24 +0100

    added an error handler. 404 page is now custom, and actually helpful for the user

---
+++ src/main/java/gruppe8/nebula/controllers/ErrorController.java
@@ -0,0 +1,18 @@
+package gruppe8.nebula.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
+
+
+    @RequestMapping("/error")
+    public String errorHandler(){
+        return "error";
+
+    }
+
+
+}
